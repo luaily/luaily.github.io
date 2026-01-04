601ad29426eddd16c0ee6774242381db115b3627
luaily <113217866+luaily@users.noreply.github.com>
2026-01-03 20:39:47 -0500

    fixes v2
    2nd attempt to fix failure to deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // 1. Tell Next.js to generate a static site (creates the ./out directory)
+  output: 'export',
+  
+  // 2. Disable server-based image optimization (required for static export)
+  images: {
+    unoptimized: true,
+  },
 };
-
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = { basePath: isProd ? '/luaily.github.io' : '', assetPrefix: isProd ? '/luaily.github.io/ : '', images: { unoptimized: true, }, };
 
 export default nextConfig;
