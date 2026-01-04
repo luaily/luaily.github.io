35b35852c5f3014254c27fc51228fa97b702c3b9
luaily <113217866+luaily@users.noreply.github.com>
2026-01-03 20:31:49 -0500

    fixes
    attempt to fix deployment issues with nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,4 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = { basePath: isProd ? '/luaily.github.io' : '', assetPrefix: isProd ? '/luaily.github.io/ : '', images: { unoptimized: true, }, };
+
 export default nextConfig;
